fd47f1343e42a169e5b3f861b2f649b04eecc6c5
Nikhil Chauhan <35319210+nikhil1699@users.noreply.github.com>
2019-02-19 21:45:31 +0530

    Added Cors

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ let bodyParser = require('body-parser');
 let mongoose = require('mongoose');
 
 let app = express();
-
+let config = require('./config');
+let cors= require('./utils/index').cors;
 let apiRoutes = require("./routes/api-routes")
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
+
 // Connect to Mongoose and set connection variable
 mongoose.connect('mongodb://localhost/resthub',{ useNewUrlParser: true });
 var db = mongoose.connection;
@@ -20,6 +22,7 @@ var port = process.env.PORT || 8080;
 // Send message for default URL
 app.get('/', (req, res) => res.send('Hello World with Express'));
 // Use Api routes in the App
+if(config.enableCors) app.use(cors);
 app.use('/api', apiRoutes)
 
 app.listen(port, function () {
