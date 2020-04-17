f891b71aa7382d96e8d17dededaa07208eb8ced2
Nikhil Chauhan <nikhil.chauhan9916@gmail.com>
2020-04-17 08:45:31 +0530

    Update contactModel.js

--- models/contactModel.js
+++ models/contactModel.js
@@ -1,7 +1,7 @@
 // contactModel.js
-var mongoose = require('mongoose');
+let mongoose = require('mongoose');
 // Setup schema
-var contactSchema = mongoose.Schema({
+let contactSchema = mongoose.Schema({
     name: {
         type: String,
         required: true
@@ -35,7 +35,7 @@ var contactSchema = mongoose.Schema({
     }
 });
 // Export Contact model
-var Contact = module.exports = mongoose.model('contact', contactSchema);
+let Contact = module.exports = mongoose.model('contact', contactSchema);
 module.exports.get = function (callback, limit) {
     Contact.find(callback).limit(limit);
 }
